181876b9e26468b1c273bf70fda52151e6d32bc5
Your Name <greiven@Greivins-MacBook-Pro.local>
2020-05-29 19:14:31 -0700

    Updates

---
+++ precache-manifest.871ec73afa9def391e771d108bd90d0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8eda7bf1573de10d3589bdeb97e5c06",
+    "url": "/react-e/index.html"
+  },
+  {
+    "revision": "d769a414d4bc219856a1",
+    "url": "/react-e/static/css/main.959dbbd7.chunk.css"
+  },
+  {
+    "revision": "a66db9ebb2bfc6eff965",
+    "url": "/react-e/static/js/2.bc67396f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-e/static/js/2.bc67396f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d769a414d4bc219856a1",
+    "url": "/react-e/static/js/main.82af6c9b.chunk.js"
+  },
+  {
+    "revision": "d2c9c3f423f6cb211d5f",
+    "url": "/react-e/static/js/runtime-main.3f49b9f0.js"
+  }
+]);
